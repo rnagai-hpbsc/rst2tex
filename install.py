f77fe3111783985d8a5cd70330e65b9d2cb3fa48
Ryo Nagai <rnagai@hepburn.s.chiba-u.ac.jp>
2024-08-26 08:24:01 +0900

    Add an install script

--- install.py
+++ install.py
@@ -1,5 +1,10 @@
 import os 
 
 homedir = os.environ['HOME']
+cdir = os.getcwd()
 
+print(cdir)
+
+with open(f"{homedir}/.bash_aliases","a") as f:
+    f.write(f"\nalias rst2tex='python3 {cdir}/rst2tex.py'")
 
